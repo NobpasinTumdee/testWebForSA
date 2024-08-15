cef32fd640cb3ee5ab01294535c627079a918630
Nichakorn Chanyutha <nichakorn391@gmail.com>
2024-08-15 19:32:14 +0700

    EditInformation

--- Frontend/myWeb/src/EditInformation/EditInformation.tsx
+++ Frontend/myWeb/src/EditInformation/EditInformation.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './EditInformation.css'; // นำเข้าไฟล์ CSS
+import './EditInformation.css';
 
 const EditInformation: FC = () => {
   const navigate = useNavigate();
 
   const handleReturnClick = () => {
-    navigate('/MainWeb'); // ปรับเส้นทางตามการตั้งค่า Routing ของคุณ
+    navigate('/MainWeb'); // เส้นทางกลับไปยังหน้า MainWeb
   };
 
   const handleEditInformation = (event: React.FormEvent<HTMLFormElement>) => {
@@ -15,26 +15,39 @@ const EditInformation: FC = () => {
   };
 
   return (
-    <div className="container">
-      <h2>แก้ไขข้อมูล</h2>
-      <form onSubmit={handleEditInformation}>
-        <div>
-          <input type="text"  />
-          <input type="text"  />
+    <div className="containerEdit">
+      <h2 className='edit-h2'>EDIT INFORMATION</h2>
+      <form className="edit-form" onSubmit={handleEditInformation}>
+        <div className="form-group">
+          <div className="input-group">
+            <label>Old USERNAME</label>
+            <input type="text" name="oldUsername" required />
+          </div>
+          <div className="input-group">
+            <label>New USERNAME</label>
+            <input type="text" name="newUsername" required />
+          </div>
         </div>
-        <div>
-          <input type="password"  />
-          <input type="password"  />
+        <div className="form-group">
+          <div className="input-group">
+            <label>Old PASSWORD</label>
+            <input type="password" name="oldPassword" required />
+          </div>
+          <div className="input-group">
+            <label>New PASSWORD</label>
+            <input type="password" name="newPassword" required />
+          </div>
         </div>
-        <div>
-          <input type="password"  />
-        </div>
-        <button type="submit">
+          <div className="input-group">
+            <label>Confirm PASSWORD</label>
+            <input type="password" name="confirmPassword" required />
+          </div>
+        <button type="submit" className="editinformation-button">
           Edit Information
         </button>
       </form>
       <button className="return-button" onClick={handleReturnClick}>
-        กลับไปหน้าหลัก
+        Return to home page
       </button>
     </div>
   );
