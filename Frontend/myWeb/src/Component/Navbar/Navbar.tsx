e5ab4bead8cdffb2ab658ea783975d7cdb869261
NobpasinTumdee <porporpor579@gmail.com>
2024-09-02 21:42:42 +0700

    update navbar

--- Frontend/myWeb/src/Component/Navbar/Navbar.tsx
+++ Frontend/myWeb/src/Component/Navbar/Navbar.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link, useLocation } from 'react-router-dom';
 import "./Navbar.css"
 // about me
 import { DataUser } from '../../AboutMe/DataUser';
 import userPhoto from './User.png';
 
 export const Navbar: React.FC = () => {
-    const navigate = useNavigate();
-    // popup
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  // popup
   const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   const openPopup = () => {
@@ -18,44 +21,45 @@ export const Navbar: React.FC = () => {
     setIsPopupOpen(false);
   };
 
-  const Edit = () => {
-    navigate("/EditInformation")
+  const Edit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    navigate("/EditInformation");
     setIsPopupOpen(false);
   }
-    return (
-        <div className="sidebarComponent">
-            <div className="logoComponent"></div>
-            <nav>
-                <ul>
-                    <a href='./MainWeb'>
-                        <li className="sizeMenuComponent">🎞️</li>
-                    </a>
-                    <a  onClick={() => openPopup()}>
-                        <li className="sizeMenuComponent">💁🏻‍♀️</li>
-                    </a>
-                    <a href='./Collection'>
-                        <li className="sizeMenuComponent">❤️</li>
-                    </a>
-                    <a href="/EditInformation" >
-                        <li className="sizeMenuComponent">👔</li>
-                    </a>
-                    <a href="/History" >
-                        <li className="sizeMenuComponent">👜</li>
-                    </a>
-                    <a href="/Admin" >
-                        <li className="sizeMenuComponent">💻</li>
-                    </a>
-                    <a href="/" >
-                        <li className="sizeMenuComponent">🔙</li>
-                    </a>
-                </ul>
-            </nav>
-        {isPopupOpen && (
+
+
+  return (
+    <div className="sidebarComponent">
+      <div className="logoComponent"></div>
+      <nav>
+        <ul>
+          {/*หาก location.pathname ตรงกับ "/EditInformation", จะเพิ่มคลาส active ให้กับไอคอนนั้น*/}
+          <Link to="/MainWeb">
+            <li className={`sizeMenuComponent ${location.pathname === "/MainWeb" ? "active" : ""}`}>🎞️</li>
+          </Link>
+          <li className={`sizeMenuComponent ${isPopupOpen ? "active" : ""}`} onClick={openPopup}>💁🏻‍♀️</li>
+          <Link to="/Collection">
+            <li className={`sizeMenuComponent ${location.pathname === "/Collection" ? "active" : ""}`}>❤️</li>
+          </Link>
+          <Link to="/EditInformation">
+            <li className={`sizeMenuComponent ${location.pathname === "/EditInformation" ? "active" : ""}`}>👔</li>
+          </Link>
+          <Link to="/History">
+            <li className={`sizeMenuComponent ${location.pathname === "/History" ? "active" : ""}`}>👜</li>
+          </Link>
+          <Link to="/Admin">
+            <li className={`sizeMenuComponent ${location.pathname === "/Admin" ? "active" : ""}`}>💻</li>
+          </Link>
+          <Link to="/">
+            <li className="sizeMenuComponent">🔙</li>
+          </Link>
+        </ul>
+      </nav>
+      {isPopupOpen && (
         <div className="popup-overlay">
           <div className="popup-content">
             {DataUser.map((User) => (
-
-              <div  key={User.id}>
+              <div key={User.id}>
                 <img src={userPhoto} className='imgAboutME' />
                 <div className='dataAboutME'>
                   <div>Name : {User.USERNAME}</div>
@@ -63,14 +67,13 @@ export const Navbar: React.FC = () => {
                   <div>Duration : {User.Duration}</div>
                   <div>Expire : {User.Expire}</div>
                 </div>
-              </div>))}
-            <button className="payment-button" onClick={() => Edit()}> Edit your Information </button>
-            <button className="close-button" onClick={() => closePopup()}> Close </button>
+              </div>
+            ))}
+            <button className="payment-button" onClick={Edit}>Edit your Information</button>
+            <button className="close-button" onClick={closePopup}>Close</button>
           </div>
         </div>
       )}
-
-        </div>
-    )
+    </div>
+  )
 }
-
