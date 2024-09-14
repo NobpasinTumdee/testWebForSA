987159817d5ec42b04002ae27d8d7ea6e391f838
NobpasinTumdee <porporpor579@gmail.com>
2024-09-15 00:33:38 +0700

    update payment

--- Frontend/myWeb/src/HistoryPayment/Payment.tsx
+++ Frontend/myWeb/src/HistoryPayment/Payment.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import HistoryPayment from './HistoryPayment';
+//import HistoryPayment from './HistoryPayment';
 import Mastercard from '../Component/Card/Mastercard';
 
 const Payment: React.FC = () => {
@@ -8,8 +8,11 @@ const Payment: React.FC = () => {
     
     return (
         <>
-         <HistoryPayment />
+         {/* <HistoryPayment /> */}
+         <h1 style={{textAlign: "center" , color: '#ffff'}} className="History-title">Payment</h1>
          <Mastercard />
+         <a href="/subscription" className="return-button-Admin">Return to subscription page</a>
+
         </>
         
     );
