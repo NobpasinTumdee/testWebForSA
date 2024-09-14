59887523c9c4b412ccb26e26b7b47dfc844b2ab3
Supaluck Tohthong <spltt.38252@gmail.com>
2024-09-14 13:59:47 +0700

    update link social

--- Frontend/myWeb/src/Component/Social/Social.tsx
+++ Frontend/myWeb/src/Component/Social/Social.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './SocialIcons.module.css';
 
-
 // popup
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -16,12 +15,13 @@ const SocialIcons: React.FC = () => {
     draggable: true,
     progress: undefined,
     theme: "light",
-    });
+  });
+
   return (
     <>
-      <ul style={{ listStyle: 'none', display: 'flex', gap: '20px', position: 'fixed', right: '15px', bottom: '0' ,zIndex: '1000'}}>
+      <ul style={{ listStyle: 'none', display: 'flex', gap: '20px', position: 'fixed', right: '15px', bottom: '0', zIndex: '1000' }}>
         <li className={`${styles.item} ${styles.first}`}>
-          <a onClick={notify}>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
             <i className={`fa-brands fa-instagram ${styles.icon}`}></i>
           </a>
         </li>
@@ -31,12 +31,12 @@ const SocialIcons: React.FC = () => {
           </a>
         </li>
         <li className={`${styles.item} ${styles.third}`}>
-          <a onClick={notify}>
+          <a href="https://www.youtube.com/watch?v=KEbQnH3UaZs" target="_blank" rel="noopener noreferrer">
             <i className={`fa-brands fa-youtube ${styles.icon}`}></i>
           </a>
         </li>
         <li className={`${styles.item} ${styles.fourth}`}>
-          <a onClick={notify}>
+          <a href="https://x.com/178tk_/status/1825880361580245059?s=61&t=YZ3gzoXN_nbOxHnGagwrfg" target="_blank" rel="noopener noreferrer">
             <i className={`fa-brands fa-x-twitter ${styles.icon}`}></i>
           </a>
         </li>
