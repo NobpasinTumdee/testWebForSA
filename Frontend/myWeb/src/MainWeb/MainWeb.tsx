a211d5457364adb47ec57143ec9b3a9197e58da1
NobpasinTumdee <porporpor579@gmail.com>
2024-09-20 12:25:11 +0700

    test sub Branch

--- Frontend/myWeb/src/MainWeb/MainWeb.tsx
+++ Frontend/myWeb/src/MainWeb/MainWeb.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect,useRef } from 'react';
-import "./MainWeb.css";
+import "./MainWeb.css";//branch testฟีเจอร์ใหม่
 
 //โปสเตอร์
 //import Xmen from "../assets/Movie/xmen.jpg"
